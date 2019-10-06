bc60591696a959983c968720f958e129ac0cec14
Merrell Gilbert <sam17me@yahoo.com>
2019-10-06 18:21:14 -0500

    problems in the mainClass, passing everything to handler and parsing database from there

--- handlers/discriptorHandler.php
+++ handlers/discriptorHandler.php
@@ -1,11 +1,26 @@
 <?php 
 require_once("../classes/mainClass.php");
 $dataIn = $_REQUEST['criterion'];
-echo $dataIn;
+//echo $dataIn;
+
+
+// make database connection here.
+$conn = new mysqli("localhost", "merrell", "47$!Lver", "adt"); 
+
+if ($result = $conn->query("SELECT `alpha`.`code` FROM `alpha` where `discriptor` LIKE '$dataIn%';";")) {
+    printf("Select returned %d rows.\n", $result->num_rows);
+
+    /* free result set */
+    $result->close();
+}
+
+/**** Test
 
 // create an instance of the mainClass
 $rubrics = new mainClass;
 
 //call the  get_discriptor function from mainClass
 $rubrics->get_discriptor($dataIn);
+
+****/
 ?>
