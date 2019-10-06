02da0aaffac1fbe8e23675fd0637ecb7ffbef04d
Merrell Gilbert <sam17me@yahoo.com>
2019-10-06 17:02:20 -0500

    updated the  handlers to pass the data to the database and get results.

--- handlers/discriptorHandler.php
+++ handlers/discriptorHandler.php
@@ -3,4 +3,9 @@ require_once("../classes/mainClass.php");
 $dataIn = $_REQUEST['criterion'];
 echo $dataIn;
 
+// create an instance of the mainClass
+$rubrics = new mainClass;
+
+//call the  get_discriptor function from mainClass
+$rubrics->get_discriptor($dataIn);
 ?>
