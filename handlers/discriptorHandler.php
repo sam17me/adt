a2a6d5f5106bc04a84c52b0f0daa190d8753f4c4
Merrell Gilbert <sam17me@yahoo.com>
2019-10-24 11:50:30 -0500

    Removed the working database query and copied it to the mainClass

--- handlers/discriptorHandler.php
+++ handlers/discriptorHandler.php
@@ -7,16 +7,6 @@ $dataIn = $_REQUEST['criterion'];
 // make database connection here.
 $conn = new mysqli("localhost", "merrell", "47$!lver", "adt"); 
 
-if ($result = $conn->query("SELECT `alpha`.`code` FROM `alpha` where `discriptor` LIKE '$dataIn%';")) {
-    
-        $row =   $result->fetch_assoc();
-	echo $row['code'];
-
-
-    /* free result set */
-    $result->close();
-}
-
 /**** Test
 
 // create an instance of the mainClass
